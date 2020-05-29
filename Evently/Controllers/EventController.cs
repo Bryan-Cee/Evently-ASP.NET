c89ae39996a723f52a4e788966e12d4afb2c63cd
Bryan-Cee <cheruiyotbryan@gmail.com>
2020-05-29 17:43:13 +0300

    Create bootstraped application with ASP.NET Core and update Bootstrap framework

---
+++ Evently/Controllers/EventController.cs
@@ -0,0 +1,31 @@
+﻿using Evently.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evently.Controllers
+{
+    public class EventController : Controller
+    {
+        public List<EventModel> events { get; set; }
+        // GET: Event
+        public ActionResult Index()
+        {
+            events = new List<EventModel>
+            {
+                new EventModel { EventName = "Microsoft Build", EventDescription = "Just a meet up for developers", EventDate = DateTime.Now },
+                new EventModel { EventName = "Google IO", EventDescription = "Google IO meet up for new things", EventDate = new DateTime(2010, 1, 1, 8, 0, 15) },
+                new EventModel { EventName = "Amazon Webinar", EventDescription = "Anything AWS", EventDate = DateTime.Now }
+            };
+
+            return View(events);
+        }
+
+        public ActionResult CreateEvent()
+        {
+            return View();
+        }
+    }
+}
---
+++ Evently/Models/EventModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evently.Models
+{
+    public class EventModel
+    {
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
